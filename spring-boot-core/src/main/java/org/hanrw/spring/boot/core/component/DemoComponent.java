f0b9e0301bf040d88e78cf6714cff4ef5415071d
slamhan <slamhan1987@hotmail.com>
2020-03-22 16:58:22 +0800

    update flow

---
+++ spring-boot-core/src/main/java/org/hanrw/spring/boot/core/component/DemoComponent.java
@@ -0,0 +1,15 @@
+package org.hanrw.spring.boot.core.component;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author hanrw
+ * @date 2020/3/22 2:58 PM
+ */
+@Data
+@Component
+public class DemoComponent {
+  private String name = "demo";
+
+}
